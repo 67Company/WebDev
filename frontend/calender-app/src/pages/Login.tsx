7c1beeebaa758ea717eed4013b77a450313cecbd
Aortus <thijs1506@gmail.com>
2025-09-18 19:02:33 +0200

    Let op nieuwe dependencies

--- frontend/calender-app/src/pages/Login.tsx
+++ frontend/calender-app/src/pages/Login.tsx
@@ -8,13 +8,13 @@ const Login: React.FC = () => {
         <h2 className="login-title">Login</h2>
         <form>
           <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Enter your email" />
+            <label htmlFor="email" className="loginlabel">Email</label>
+            <input type="email" id="email" placeholder="Enter email" />
           </div>
 
           <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" />
+            <label htmlFor="password" className="loginlabel">Password</label>
+            <input type="password" id="password" placeholder="Enter password" />
           </div>
 
           <button type="submit" className="login-button">Login</button>
