019a219dc961e7af419905ba0e66fcdbe7b0a935
Shrimpey <kimo.edelbroek@gmail.com>
2025-10-30 11:23:51 +0100

    fix: zindex was wrong for time indicator

--- backend/calender-backend/obj/Debug/net9.0/calender-backend.AssemblyInfo.cs
+++ backend/calender-backend/obj/Debug/net9.0/calender-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calender-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c651e16ab97ae1878fd9eb74eb68e6179b8fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b633299e9388cfd9d151436b6175fec60fd4253")]
 [assembly: System.Reflection.AssemblyProductAttribute("calender-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calender-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
