782e28d85d9c2511ff70e25942ed827e91c3913d
Aortus <thijs1506@gmail.com>
2025-11-11 16:52:17 +0100

    Wow such pretty omigosh

--- frontend/calender-app/src/pages/Home.tsx
+++ frontend/calender-app/src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from '@mui/material/Button';
 import "../styles/Home.css";
 import "../styles/Cards.css";
 
@@ -9,7 +8,7 @@ const Home: React.FC = () => {
   return (
     <main className="App-main">
       <section className="overview-card">
-        <h2>📅 Calendar</h2>
+        <h2>Calendar</h2>
         <p>Take a look at your calendar here.</p>
         <Link to="/Calendar">
         <button className="button">Click for more</button>
@@ -17,17 +16,17 @@ const Home: React.FC = () => {
       </section>
 
       <section className="overview-card">
-        <h2>⚙️ Settings</h2>
-        <p>Manage your preferences and configurations.</p>
-        <Link to="/Settings">
+        <h2>Achievements</h2>
+        <p>Need help? Reach out to our support team anytime.</p>
+        <Link to="/Achievements">
         <button className="button">Click for more</button>
         </Link>
       </section>
-
+      
       <section className="overview-card">
-        <h2>👀 Achievements</h2>
-        <p>Need help? Reach out to our support team anytime.</p>
-        <Link to="/Achievements">
+        <h2>Settings</h2>
+        <p>Manage your preferences and configurations.</p>
+        <Link to="/Settings">
         <button className="button">Click for more</button>
         </Link>
       </section>
--- frontend/calender-app/src/pages/Settings.tsx
+++ frontend/calender-app/src/pages/Settings.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "../styles/Settings.css";
+import "../styles/Cards.css";
 import ThemeToggle from "../components/ThemeToggle";
-import AchievementButton from "../components/AchievementButton";
 
 const Settings: React.FC = () => {
   return (
     <main className="App-main">
-      <section className="settings-card">
+      <section className="overview-card">
         <h2>⚙️ Settings</h2>
         <p>Manage your preferences and configurations.</p>
 
         <div className="setting-item">
-          <h3>Theme</h3>
+          <h2>Theme</h2>
           <ThemeToggle />
           <p>Switch between light and dark mode.</p>
         </div>
-      <AchievementButton/>
 
       </section>
     </main>
