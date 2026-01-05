9fe7ed7929c0adf594974e2e6be9f2f67d020a27
Aortus <thijs1506@gmail.com>
2026-01-05 14:16:06 +0100

    remove not needed import

--- backend/calender-backend/Filters/AdminOnlyAttribute.cs
+++ backend/calender-backend/Filters/AdminOnlyAttribute.cs
@@ -1,11 +1,5 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-
 namespace calender_backend.Filters;
 
-/// <summary>
-/// Simple session-based admin gate. Returns 403 when the current session is not marked as admin.
-/// </summary>
 public class AdminOnlyAttribute : Attribute, IAsyncAuthorizationFilter
 {
     public Task OnAuthorizationAsync(AuthorizationFilterContext context)
